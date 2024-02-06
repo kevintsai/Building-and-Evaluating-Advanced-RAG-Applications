3a9cf885627b46aed7e8555d049eedaa83a34852
potus84 <tuantai.cse@gmail.com>
2024-02-06 10:37:41 +0100

    Reorder the files in the repository to make it more organized.

--- utils.py
+++ utils.py
@@ -5,11 +5,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 import numpy as np
-from trulens_eval import (
-    Feedback,
-    TruLlama,
-    OpenAI
-)
+from trulens_eval import Feedback, TruLlama, OpenAI
 
 from trulens_eval.feedback import Groundedness
 import nest_asyncio
@@ -28,47 +24,42 @@ def get_hf_api_key():
 
     return os.getenv("HUGGINGFACE_API_KEY")
 
+
 openai = OpenAI()
 
-qa_relevance = (
-    Feedback(openai.relevance_with_cot_reasons, name="Answer Relevance")
-    .on_input_output()
-)
+qa_relevance = Feedback(
+    openai.relevance_with_cot_reasons, name="Answer Relevance"
+).on_input_output()
 
 qs_relevance = (
-    Feedback(openai.relevance_with_cot_reasons, name = "Context Relevance")
+    Feedback(openai.relevance_with_cot_reasons, name="Context Relevance")
     .on_input()
     .on(TruLlama.select_source_nodes().node.text)
     .aggregate(np.mean)
 )
 
-#grounded = Groundedness(groundedness_provider=openai, summarize_provider=openai)
+# grounded = Groundedness(groundedness_provider=openai, summarize_provider=openai)
 grounded = Groundedness(groundedness_provider=openai)
 
 groundedness = (
     Feedback(grounded.groundedness_measure_with_cot_reasons, name="Groundedness")
-        .on(TruLlama.select_source_nodes().node.text)
-        .on_output()
-        .aggregate(grounded.grounded_statements_aggregator)
+    .on(TruLlama.select_source_nodes().node.text)
+    .on_output()
+    .aggregate(grounded.grounded_statements_aggregator)
 )
 
 feedbacks = [qa_relevance, qs_relevance, groundedness]
 
+
 def get_trulens_recorder(query_engine, feedbacks, app_id):
-    tru_recorder = TruLlama(
-        query_engine,
-        app_id=app_id,
-        feedbacks=feedbacks
-    )
+    tru_recorder = TruLlama(query_engine, app_id=app_id, feedbacks=feedbacks)
     return tru_recorder
 
+
 def get_prebuilt_trulens_recorder(query_engine, app_id):
-    tru_recorder = TruLlama(
-        query_engine,
-        app_id=app_id,
-        feedbacks=feedbacks
-        )
+    tru_recorder = TruLlama(query_engine, app_id=app_id, feedbacks=feedbacks)
     return tru_recorder
+
 
 from llama_index import ServiceContext, VectorStoreIndex, StorageContext
 from llama_index.node_parser import SentenceWindowNodeParser
